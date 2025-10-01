d53f4f11f33d768b118fed095de7267434899aba
slity <0911kjw@yu.ac.kr>
2025-10-01 11:21:48 +0900

    chore: 초기 프로젝트 설정
    - tailwindcss
    - axios
    - router
    - react-icons

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // port 고정 
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+})
