f3fdaae9f29a6afc83744e127c1cd7ddb5f2c7a2
vni911 <0911kjw@naver.com>
2025-10-09 03:53:27 +0900

    feat: 게시물 페이지 헤더 적용
    - 게시물 페이지 헤더 적용

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import HeaderLoggedIn from '../components/header/HeaderLoggedIn'
 
 function Posts() {
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-semibold">Posts</h1>
-      <p>게시물 목록 페이지</p>
+    <div className="min-h-screen bg-gray-50">
+      <HeaderLoggedIn />
+      <div className="p-4">
+        <h1 className="text-xl font-semibold">Posts</h1>
+        <p>게시물 목록 페이지</p>
+      </div>
     </div>
   )
 }
