d0112a6f5c3a53adc497c9b5d2ce84207c959367
hyeji-neee <tlsgmlsk123@naver.com>
2025-10-09 02:37:40 +0900

    feat: 로그인/비로그인/회원가입 화면별 헤더 표시 구현

--- src/components/header/HeaderLoggedOut.jsx
+++ src/components/header/HeaderLoggedOut.jsx
@@ -5,19 +5,19 @@ import { Link } from "react-router-dom";
 const HeaderLoggedOut = () => {
   return (
     <div className="flex items-center justify-between w-full h-[60px]">
-      <Link className="flex items-center space-x-1 pl-[100px]" to="/landing">
+      <Link className="flex items-center space-x-1 pl-[80px]" to="/landing">
         <img src={logo} alt="logo" className="w-[60px] h-[60px]" />
         <div className="text-[20px] font-semibold">Code Gardener</div>
       </Link>
 
-      <div className="flex items-center space-x-20 text-[14px] font-medium">
+      <div className="flex-1 pl-[280px] flex items-center space-x-20 text-[14px] font-medium">
         <Link to="/posts">코드 정원</Link>
         <Link to="/upload">코드 심기</Link>
         <Link to="/leader-board">리더보드</Link>
         <Link to="*">메뉴4</Link>
       </div>
 
-      <div className="flex space-x-8 pr-[120px]">
+      <div className="flex space-x-8 pr-28">
         <Link
           className="flex justify-center items-center text-[12px]"
           to="/sign-in"
