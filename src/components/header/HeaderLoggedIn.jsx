5ba53bb565a9e8247f21b3bde27efb60424d6902
vni911 <0911kjw@naver.com>
2025-10-09 04:18:12 +0900

    fix: 로그인 후 헤더 디자인 변경
    - 로그인 후 헤더 디자인 변경
     + 색상 흰색 고정, 하단 border 생성

--- src/components/header/HeaderLoggedIn.jsx
+++ src/components/header/HeaderLoggedIn.jsx
@@ -6,7 +6,7 @@ import { IoPersonCircle } from "react-icons/io5";
 // 로그인한 사용자용 헤더
 const HeaderLoggedIn = () => {
   return (
-    <div className="flex items-center justify-between w-full h-[60px]">
+    <div className="flex items-center justify-between w-full h-[60px] bg-white border-b-2">
       <Link className="flex items-center space-x-1 pl-[80px]" to="/main">
         <img src={logo} alt="logo" className="w-[60px] h-[60px]" />
         <div className="text-[20px] font-semibold">Code Gardener</div>
