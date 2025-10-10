7841427fcc7b3ee3562f6f4885333a69e7ead8c1
hyeji-neee <tlsgmlsk123@naver.com>
2025-10-11 02:36:48 +0900

    feat: 로그인 페이지 헤더 및 배경 적용

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/header/Header";
+import illustration from "../assets/illustration.png";
 
 function SignIn() {
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-semibold">Sign In</h1>
-      <p>로그인 페이지</p>
+    <div className="w-full h-screen bg-gradient-to-r from-[#F0FDF4] to-[#BDF7D1]">
+      <Header />
+      <div className="flex justify-center">
+        <div className="mt-20">
+          <div className="text-[30px] font-semibold">
+            <h3 className="text-[#4D4D4D]">
+              안녕하세요, 코드와 피드백이 자라는 정원
+            </h3>
+            <span className="text-[#00C839]">Code Gardener</span>
+            <span className="text-[#4D4D4D]">입니다.</span>
+          </div>
+          <img
+            src={illustration}
+            alt="illust"
+            className="w-[673px] h-[449px]"
+          />
+        </div>
+
+        <div className="w-[499px] h-[562px] bg-white rounded-[16px] border border-[#B8B8B8] mt-12">
+          <h3>로그인</h3>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignIn
-
-
+export default SignIn;
