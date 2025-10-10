29835f889b0fa647bd59418a794e424cc6c2a6f6
hyeji-neee <tlsgmlsk123@naver.com>
2025-10-11 02:59:39 +0900

    feat: 로그인 폼 영역 구현

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -22,8 +22,33 @@ function SignIn() {
           />
         </div>
 
-        <div className="w-[499px] h-[562px] bg-white rounded-[16px] border border-[#B8B8B8] mt-12">
-          <h3>로그인</h3>
+        <div className="flex flex-col items-center w-[499px] h-[562px] bg-white rounded-[16px] border border-[#B8B8B8] mt-12">
+          <h2 className="text-[30px] font-semibold mt-16 mb-12">로그인</h2>
+          <div className="flex flex-col items-center">
+            <input
+              type="text"
+              placeholder="아이디를 입력해 주세요"
+              className="w-[360px] h-[48px] border border-[#B8B8B8] rounded-[6px] px-4 py-3 text-[14px] focus:outline-none mb-4 placeholder:text-[#B8B8B8]"
+            />
+            <input
+              type="password"
+              placeholder="비밀번호를 입력해 주세요"
+              className="w-[360px] h-[48px] border border-[#B8B8B8] rounded-[6px] px-4 py-3 text-[14px] focus:outline-none mb-8 placeholder:text-[#B8B8B8]"
+            />
+            <button className="w-[160px] h-[40px] bg-[#00C839] text-white text-[16px] py-2 rounded-[6px] font-medium hover:bg-[#0bdd47]">
+              로그인
+            </button>
+          </div>
+
+          <p className="text-[10px] text-[#B8B8B8] mt-4">
+            계정이 없으신가요?{" "}
+            <a
+              href="/sign-up"
+              className="text-[#00C839] font-semibold hover:underline"
+            >
+              회원가입
+            </a>
+          </p>
         </div>
       </div>
     </div>
