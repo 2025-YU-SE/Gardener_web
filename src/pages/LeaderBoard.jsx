8066ffecd14c083e2a4a660c25d585a6d5a51c88
hyeji-neee <tlsgmlsk123@naver.com>
2025-10-12 20:02:48 +0900

    feat: 헤더 적용 및 리더보드 타이틀/소개 텍스트 추가

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/header/Header";
 
 function LeaderBoard() {
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-semibold">Leader Board</h1>
-      <p>리더보드 페이지</p>
+    <div className="min-h-screen bg-[#F9FAFB]">
+      <Header />
+      <div>
+        {/* 타이틀 + 소개 텍스트 */}
+        <div className="text-center mt-8">
+          <h1 className="text-[30px] font-semibold">리더보드</h1>
+          <p className="mt-2 text-[14px] text-[#4D4D4D]">
+            Code Gardener에서 가장 활발한 가드너들을 만나보세요
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LeaderBoard
-
-
+export default LeaderBoard;
