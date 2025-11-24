c395d918818dd3376aea89ca27cc41c4139f4272
slity <0911kjw@yu.ac.kr>
2025-11-24 17:12:06 +0900

    chore : API 주소 환경변수로 변경
    - API 주소 환경변수로 변경 및 서버 연결

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // port 고정 
-    open: true,
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const { VITE_API_BASE_URL = 'http://localhost:8080' } = env
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      open: true,
+      proxy: {
+        '/api': VITE_API_BASE_URL,
+      },
+    },
+  }
 })
